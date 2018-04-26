fa3fbb95e09ec6e347e7ac744619386f0b50dc4d
Allan Jones <iamallanjones@gmail.com>
2018-04-25 21:55:13 -0500

    Updated Customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@
 // = = = = = = = = = = = = = = = = =
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Table = require("easy-table");
 
 // = = = = = = = = = = = = = = = = =
 // DB Connection
@@ -16,7 +17,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "", //Enter PW
+  password: "Babetoba0916!", //Enter PW
   database: "bamazon"
 });
 
@@ -24,3 +25,45 @@ connection.connect(function(err) {
   if (err) throw err;
   displayItems();
 });
+
+function displayItems() {
+  // Display Table of Products in CLI
+  var query = "SELECT * FROM products";
+  connection.query(query, function(err, res) {
+    let data = res;
+    let t = new Table();
+
+    data.forEach(function(product) {
+      t.cell("Product Id", product.item_id);
+      t.cell("Product Name", product.product_name);
+      t.cell("Price, USD", product.price, Table.number(2));
+      t.newRow();
+    });
+
+    console.log(t.toString());
+  });
+}
+// Ask Customer Product ID
+function askId() {
+  inquirer
+    .prompt({
+      name: "productId",
+      type: "input",
+      message: "What is the ID of the product you'd like to buy?"
+    })
+    .then(function(answer) {
+      var query = "SELECT * FROM products WHERE ?";
+      connection.query(query, { item_id: answer.item_id }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Item ID: " +
+              res[0].item_id +
+              " || Product Name: " +
+              res[0].product_name +
+              " || Price: " +
+              res[0].price
+          );
+        }
+      });
+    });
+}
