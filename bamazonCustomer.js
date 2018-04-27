92c5effc2397615bc2c851ae30adf04ac3c26986
Allan Jones <iamallanjones@gmail.com>
2018-04-27 03:25:22 -0500

    Update DB with inserted values/update questions for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,10 +6,10 @@ const inquirer = require("inquirer");
 const Table = require("easy-table");
 
 // = = = = = = = = = = = = = = = = =
-// DB Connection
+// Create Connection info for the sql db
 // = = = = = = = = = = = = = = = = =
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
@@ -21,19 +21,27 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+// = = = = = = = = = = = = = = = = =
+// Connect to the mysql server and sql database
+// = = = = = = = = = = = = = = = = =
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("~ ~ ~ ~ ~ WELCOME TO BAMAZON  ~ ~ ~ ~ ~ ");
-  displayItems();
+  displayInventory();
+  // run the start function after the connection is made to prompt the user
+  start();
 });
 
-function displayItems() {
-  // Display Table of Products in CLI
-  var query = "SELECT * FROM products";
+// = = = = = = = = = = = = = = = = =
+// Display Product Inventory Table
+// = = = = = = = = = = = = = = = = =
+
+function displayInventory() {
+  let query = "SELECT * FROM products";
   connection.query(query, function(err, res) {
     let data = res;
     let t = new Table();
-
     data.forEach(function(product) {
       t.cell("Product Id", product.item_id);
       t.cell("Product Name", product.product_name);
@@ -42,17 +50,23 @@ function displayItems() {
     });
     console.log(t.toString());
   });
+  askId();
 }
 
-askId();
-// Ask Customer Product ID
+// = = = = = = = = = = = = = = = = =
+//Ask Product Id - Customer 
+// = = = = = = = = = = = = = = = = =
+
 function askId() {
+  // prompt for info about the purchasing an item
   inquirer
-    .prompt({
-      name: "productId",
-      type: "input",
-      message: "What is the ID of the product you'd like to buy?"
-    })
+    .prompt([
+      {
+        name: "productId",
+        type: "input",
+        message: "What is the ID of the product you'd like to buy?"
+      }
+    ])
     .then(function(answer) {
       var query = "SELECT * FROM products WHERE ?";
       connection.query(query, { item_id: answer.item_id }, function(err, res) {
@@ -68,4 +82,38 @@ function askId() {
         }
       });
     });
+    askQty();
 }
+
+/ = = = = = = = = = = = = = = = = =
+//Ask Product Id - Customer 
+// = = = = = = = = = = = = = = = = =
+
+function askQty() {
+  // prompt for info about the purchasing an item
+  inquirer
+    .prompt([
+      {
+        name: "productQty",
+        type: "input",
+        message: "How many would you like to buy?"
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT * FROM products WHERE ?";
+      connection.query(query, { stock_qty: answer.stock_qty }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Item ID: " +
+              res[0].item_id +
+              " || Product Name: " +
+              res[0].product_name +
+              " || Price: " +
+              res[0].price
+          );
+        }
+      });
+    });
+    askQty();
+}
+
--- schema.sql
+++ schema.sql
@@ -3,12 +3,30 @@ CREATE database bamazon;
 USE bamazon;
 CREATE TABLE products
 (
-  item_id INT NOT NULL,
-  product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
-  price DECIMAL(5,2) NULL,
+  item_id INT NOT NULL
+  AUTO_INCREMENT,
+  product_name VARCHAR
+  (100) NULL,
+  department_name VARCHAR
+  (100) NULL,
+  price DECIMAL
+  (5,2) NULL,
   stock_qty INT NULL,
-  PRIMARY KEY (item_id)
+  PRIMARY KEY
+  (item_id)
 );
-SELECT *
-FROM products;
+  INSERT INTO products
+    (product_name, department_name, price, stock_qty)
+  VALUES
+    ('Atari Graphic Tee', 'Men', 12.99, 25),
+    ('Floral Print Short Sleeve Dress', 'Women', 24.99, 37),
+    ('LEGO Marvel Iron Man', 'Toy', 9.99, 5),
+    ('DJI Mavic Pro Dron', 'Electronics', 999.99, 3),
+    ('Elite Cushion Crew - Socks', 'Men', 16, 55),
+    ('Huffy Cruiser Bike', 'Outdoors', 129.75, 20),
+    ('Strappy Maxi Dress', 'Women', 29.99, 14),
+    ('Bob Marley Short Sleeve Tee', 'Men', 12.99, 8),
+    ('Cassandra Modern Fabric Queen-Bed', 'Home', 193.59, 3),
+    ('Holmes Visipure Tower Air Purifier', 'Home', 69.99, 27);
+  SELECT *
+  FROM products;
